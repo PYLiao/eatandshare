bdd48e678dec1428f8ff9c1baa11346361a5d728
PYLiao <lby75429@gmail.com>
2015-11-01 21:25:29 +0000

    Initial commit of eatandshare

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eatandshare::Application.config.secret_key_base = '4cb25dc4b61d6f06260dd8bc9e5c9fecfd036244dedab4c7545b5305160dda50e2bc5f8970a15e313cdc79fee7234d13cabb72119fd38f4d31f1d109404b283c'
