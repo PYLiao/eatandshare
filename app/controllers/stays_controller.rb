1c3b7dc9a333cd435e0044ce1aabde7a9624219c
PYLiao <lby75429@gmail.com>
2015-11-03 17:12:39 +0000

    add create and redirect

--- app/controllers/stays_controller.rb
+++ app/controllers/stays_controller.rb
@@ -6,4 +6,15 @@ class StaysController < ApplicationController
 	def new
 		@stay = Stay.new
 	end
+
+	def create
+		Stay.create(stay_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def stay_params
+		params.require(:stay).permit(:name, :description, :address)
+	end
 end
