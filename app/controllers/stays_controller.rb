9fe496169f8e04fb4425a48a1aed9bf10e52d90e
PYLiao <lby75429@gmail.com>
2015-11-03 05:15:17 +0000

    create form page and install simple form

--- app/controllers/stays_controller.rb
+++ app/controllers/stays_controller.rb
@@ -2,4 +2,8 @@ class StaysController < ApplicationController
 	def index
 		@stays = Stay.order("name").page(params[:page]).per(4)
 	end
+
+	def new
+		@stay = Stay.new
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Eatandshare::Application.routes.draw do
 
   root 'stays#index'
+
+  resources :stays
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
