94fec4036dbd3ad95fadf4757a91ac212998e0f2
PYLiao <lby75429@gmail.com>
2015-11-09 22:50:10 +0000

    add photo modal but meet a problem in index

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,14 @@
 class PhotosController < ApplicationController
 	before_action :authenticate_usermodel!
 	def create
-		@stay = Stay.find(params[:stay.id])
-		@stay.photos.create(photo_params.merge(:usermodel => current_usermodel))
+		@stay = Stay.find(params[:stay_id])
+		@stay.photos.create(photo_params)
 		redirect_to stay_path(@stay)
 	end
 
 	private
 
 	def photo_params
-		params.require(:photo).permit(:caption)
+		params.require(:photo).permit(:caption, :picture, :usermodel_id)
 	end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ActiveRecord::Base
+	mount_uploader :picture, PictureUploader
 	belongs_to :usermodel
 	belongs_to :stay
 end
