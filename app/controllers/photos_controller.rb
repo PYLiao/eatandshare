5de27e21941119fcfc3fa018d689a25a23bcd914
PYLiao <lby75429@gmail.com>
2015-11-09 20:29:06 +0000

    make migrate mistake

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,14 @@
 class PhotosController < ApplicationController
+	before_action :authenticate_usermodel!
+	def create
+		@stay = Stay.find(params[:stay.id])
+		@stay.photos.create(photo_params.merge(:usermodel => current_usermodel))
+		redirect_to stay_path(@stay)
+	end
+
+	private
+
+	def photo_params
+		params.require(:photo).permit(:caption)
+	end
 end
