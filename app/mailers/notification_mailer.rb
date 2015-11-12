0767384d98fbdba21d6c8856a26b826abc1ad94e
PYLiao <lby75429@gmail.com>
2015-11-12 05:27:25 +0000

    adjust automated email mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@eatandshareapp.com"
 
-  def comment_added
-  	mail(to: "lby75429@gmail.com",
-  		 subject: "A comment has been added to your place")
+  def comment_added(comment)
+  	@stay = comment.stay
+  	@stay_owner = @stay.usermodel
+  	mail(to: @stay_owner.email,
+  		 subject: "A comment has been added to your #{@stay.name}")
   end
 end
