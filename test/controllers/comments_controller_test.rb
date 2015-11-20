61234938801375c1f4717175fd0d3a144abea4bd
PYLiao <lby75429@gmail.com>
2015-11-20 18:53:49 +0000

    test userdashboard route & test add comment into database and redirect

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,4 +4,16 @@ class CommentsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "create comment" do
+  	user = FactoryGirl.create(:usermodel)
+  	sign_in user
+  	stay = FactoryGirl.create(:stay)
+  	assert_difference 'stay.comments.count' do
+  		post :create, :stay_id => stay.id, :comment => {
+  		:message => 'Awesome', :rating => '5_stars'
+  		}
+  	end
+  	assert_redirected_to stay_path(stay)
+  end
 end
--- test/controllers/usermodels_controller_test.rb
+++ test/controllers/usermodels_controller_test.rb
@@ -4,4 +4,13 @@ class UsermodelsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+    test "show userdashboard" do
+  		user = FactoryGirl.create(:usermodel)
+  		sign_in user
+  		get :show, :id => user.id
+  		assert_response :success
+  	end
+
+ 
 end
